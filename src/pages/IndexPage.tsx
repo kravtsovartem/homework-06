d848ddccf62b9f85be0beef7741268e88c51a936
A.Kravtsov <a.kravtsov@globexit.ru>
2025-03-19 21:32:55 +0300

    feat: Автоматический переход к последней существующей заметке

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,7 +1,23 @@
+import useNotes from '@/hooks/useNotes'
+import { Text } from '@mantine/core'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function IndexPage() {
+  const { notes } = useNotes()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (notes?.length && notes?.length != 0) {
+      navigate(`/note/${notes[notes.length - 1].id}`)
+    }
+  }, [notes])
+
   return (
     <div>
+      <Text size="xl" fw={700}>
+        Создаёте новую заметку
+      </Text>
     </div>
   )
 }
