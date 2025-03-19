6235e97822a4a34d7847ed44ae215d50f2912e54
A.Kravtsov <a.kravtsov@globexit.ru>
2025-03-19 22:58:30 +0300

    feat: Добавлена форма удаления заметки

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import AuthProvider from '@/contexts/AuthProvider'
 import ViewRouter from '@/router'
 import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
 
 import { createTheme, MantineProvider } from '@mantine/core'
+import { ModalsProvider } from '@mantine/modals'
+import { Notifications } from '@mantine/notifications'
 
 const theme = createTheme({})
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <AuthProvider>
+      <ModalsProvider>
+        <Notifications />
+        <AuthProvider>
           <ViewRouter />
-      </AuthProvider>
+        </AuthProvider>
+      </ModalsProvider>
     </MantineProvider>
   )
 }
--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -1,47 +1,72 @@
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 import { Button, Group, Space, TextInput } from '@mantine/core'
 import useNotes from '@/hooks/useNotes'
 import MarkdownEditor from '@/components/MarkdownEditor'
+import { modals } from '@mantine/modals'
+import { notifications } from '@mantine/notifications'
 
 function NotePage() {
-  const [isEdit, setEdit] = useState(true)
+  const [isEdit, setEdit] = useState(false)
 
   const params = useParams()
-	const navigate = useNavigate()
+  const navigate = useNavigate()
 
   const id = Number(params?.id)
 
   const { note, updateTextNote, updateNameNote, removeNote } = useNotes(id)
+
+  const openModal = () =>
+    modals.openConfirmModal({
+      title: 'Удалить заметку?',
+      labels: { confirm: 'Удалить', cancel: 'Отменить' },
+      confirmProps: { color: 'red' },
+      onCancel: () => {},
+      onConfirm: () => {
+        notifications.show({
+          message: 'Заметка удалена',
+          position: 'top-right',
+        })
+        navigate('/')
+        removeNote(id)
+      },
+    })
 
   const handleChangeEditMode = () => {
     setEdit((prevState) => !prevState)
   }
 
   const handleRemoveNote = () => {
-		navigate('/')
-    removeNote(id)
+    openModal()
   }
 
-	const handleUpdateTextNote = (text: string) => {
-		updateTextNote(id, text)
-	}
+  const handleUpdateTextNote = (text: string) => {
+    updateTextNote(id, text)
+  }
 
-	const handleUpdateNameNote = (name: string) => {
-		updateNameNote(id, name)
-	}
+  const handleUpdateNameNote = (name: string) => {
+    updateNameNote(id, name)
+  }
 
   return (
     <>
-      <Group>
-        <Button onClick={handleChangeEditMode}>Редактировать</Button>
-        <Button onClick={handleRemoveNote}>Удалить</Button>
-      </Group>
-      <Space h="xl" />
-      <TextInput value={note?.name} placeholder="Введите название" onChange={(e) => handleUpdateNameNote(e.target.value)} />
-      <Space h="xl" />
-      {note && <MarkdownEditor text={note?.text} isEdit={isEdit} onChange={handleUpdateTextNote} />}
+      {note && (
+        <Suspense>
+          <Group>
+            <Button onClick={handleChangeEditMode}>Редактировать</Button>
+            <Button onClick={handleRemoveNote}>Удалить</Button>
+          </Group>
+          <Space h="xl" />
+          <TextInput
+            value={note?.name}
+            placeholder="Введите название"
+            onChange={(e) => handleUpdateNameNote(e.target.value)}
+          />
+          <Space h="xl" />
+          <MarkdownEditor text={note?.text} isEdit={isEdit} onChange={handleUpdateTextNote} />
+        </Suspense>
+      )}
     </>
   )
 }
