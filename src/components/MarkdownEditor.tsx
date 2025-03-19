ae0a075e74bcce72e8d7fc86146723bad6d20d44
A.Kravtsov <a.kravtsov@globexit.ru>
2025-03-19 21:22:59 +0300

    feat: Добавлен компонент с редактором Markdown

---
+++ src/components/MarkdownEditor.tsx
@@ -0,0 +1,88 @@
+import {
+  MDXEditor,
+  toolbarPlugin,
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  diffSourcePlugin,
+  DiffSourceToggleWrapper,
+  MDXEditorMethods,
+  BoldItalicUnderlineToggles,
+  BlockTypeSelect,
+  CodeToggle,
+  InsertImage,
+  imagePlugin,
+  InsertTable,
+  Separator,
+  tablePlugin,
+  markdownShortcutPlugin,
+} from '@mdxeditor/editor'
+import '@mdxeditor/editor/style.css'
+import { useEffect, useRef } from 'react'
+
+interface IMarkdownEditorProps {
+  text: string | undefined
+  isEdit?: boolean
+  onChange?: (text: string) => void
+}
+
+export default function MarkdownEditor({ text, isEdit = false, onChange }: IMarkdownEditorProps) {
+  const editor = useRef<MDXEditorMethods>(null)
+
+  useEffect(() => {
+    if (editor.current) {
+      editor.current.setMarkdown(text ?? '')
+    }
+  }, [text])
+
+  return (
+    <div style={{ zIndex: 102 }}>
+      <MDXEditor
+        ref={editor}
+        readOnly={!isEdit}
+        markdown={text ?? ''}
+        onChange={onChange}
+        plugins={[
+          headingsPlugin(),
+          listsPlugin(),
+          quotePlugin(),
+          thematicBreakPlugin(),
+          imagePlugin(),
+          tablePlugin(),
+          diffSourcePlugin({ viewMode: 'rich-text' }),
+          markdownShortcutPlugin(),
+          toolbarPlugin({
+            toolbarClassName: 'my-classname',
+            toolbarContents: () => (
+              <>
+                <DiffSourceToggleWrapper options={['source']}>
+                  <BlockTypeSelect />
+                  <BoldItalicUnderlineToggles />
+                  <InsertImage />
+                  <CodeToggle />
+                  <Separator />
+                  <InsertTable />
+                </DiffSourceToggleWrapper>
+              </>
+            ),
+          }),
+        ]}
+      />
+    </div>
+  )
+}
+// import Markdown from 'marked-react'
+
+// const style = {
+// 	textArea: {
+// 		minWidth: '100%',
+// 		maxWidth: '100%',
+// 	}
+// }
+
+// export default function MarkdownEditor({ text, isEdit = false }: IMarkdownEditorProps) {
+//   if (isEdit) return <textarea style={style.textArea}>{text}</textarea>
+
+//   return <Markdown>{text}</Markdown>
+// }
