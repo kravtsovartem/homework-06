0d064d09dbc68ba5e1a1937f0e4f45d237ff7de5
A.Kravtsov <a.kravtsov@globexit.ru>
2025-03-20 20:25:55 +0300

    feat: Добавлено выделение активной заметки

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,27 @@
 import ListItem from '@/components/ListItem'
 import useNotes from '@/hooks/useNotes'
+import { useParams } from 'react-router-dom'
 
 function Sidebar() {
   const { notes } = useNotes()
+  const params = useParams()
 
   return (
     <div>
-      {notes?.map((note: INote) => (
-        <ListItem key={note.id} id={note.id} name={note.name.trim().length > 0 ? note.name : "Без названия"} />
-      ))}
+      <div
+        style={{
+          borderTop: '1px solid var(--app-shell-border-color)',
+        }}
+      >
+        {notes?.map((note: INote) => (
+          <ListItem
+            active={note.id === Number(params?.id)}
+            key={note.id}
+            name={note.name.trim().length > 0 ? note.name : 'Без названия'}
+            to={`/note/${note.id}`}
+          />
+        ))}
+      </div>
     </div>
   )
 }
